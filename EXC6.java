63012713d5e5b1fc1be637ad9c88aa4174746820
paragmorajkar <99032730+paragmorajkar@users.noreply.github.com>
2022-03-10 20:10:08 +0530

    Update EXC6.java

--- EXC6.java
+++ EXC6.java
@@ -1,5 +1,49 @@
 
 public class EXC6 {
+	public class BankAccount {
+
+    int accountNumber; 
 	
+    double accountBalance;
+
+
+    // to withdraw funds from the account
+    public synchronized boolean transfer (double amount)  
+    {
+        double newAccountBalance;
+
+        if( amount > accountBalance)
+	{
+             //there are not enough funds in the account
+             return false; 
+	}
+		
+	else
+	{
+            newAccountBalance = accountBalance - amount;
+            accountBalance = newAccountBalance;
+            return true;
+        }
+
+    }
+
+    public synchronized boolean deposit(double amount) 
+    {
+	double newAccountBalance;
+
+	if( amount < 0.0)
+	{
+          return false; // can not deposit a negative amount
+	}
+		
+	else
+	{
+          newAccountBalance = accountBalance + amount;
+          accountBalance = newAccountBalance;
+	  return true;
+         }
+
+     }
+
 
 }
